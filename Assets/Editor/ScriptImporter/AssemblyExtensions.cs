a1dfbdc60814f83ab34f6df95e6701a31cfe162c
hal1932 <yu.arai.19@gmail.com>
2016-10-30 03:23:37 +0900

    変数名変更

--- Assets/Editor/ScriptImporter/AssemblyExtensions.cs
+++ Assets/Editor/ScriptImporter/AssemblyExtensions.cs
@@ -6,25 +6,25 @@ namespace ScriptImporter
 {
     public static class AssemblyExtensions
     {
-        public static object ConstructInstance(this Assembly assembly, string className, params object[] args)
+        public static object ConstructInstance(this Assembly assembly, string typeName, params object[] args)
         {
             var argTypes = (args.Length > 0) ? args.Select(x => x.GetType()).ToArray() : Type.EmptyTypes;
-            return assembly.GetType(className)
+            return assembly.GetType(typeName)
                 .GetConstructor(argTypes)
                 .Invoke(args);
         }
 
-        public static object InvokeMethod(this Assembly assembly, string className, string methodName, params object[] args)
+        public static object InvokeMethod(this Assembly assembly, string typeName, string methodName, params object[] args)
         {
-            var type = assembly.GetType(className);
-            var instance = assembly.ConstructInstance(className);
+            var type = assembly.GetType(typeName);
+            var instance = assembly.ConstructInstance(typeName);
             return type.GetMethod(methodName)
                 .Invoke(instance, args);
         }
 
-        public static object InvokeStaticMethod(this Assembly assembly, string className, string methodName, params object[] args)
+        public static object InvokeStaticMethod(this Assembly assembly, string typeName, string methodName, params object[] args)
         {
-            return assembly.GetType(className)
+            return assembly.GetType(typeName)
                 .GetMethod(methodName)
                 .Invoke(null, args);
         }
